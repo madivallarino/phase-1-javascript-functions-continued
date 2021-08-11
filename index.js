c5ceb0ef17b20ca9b3380324d192043c5c7c7100
Madison Vallarino <madivallarino@gmail.com>
2021-08-10 22:35:57 -0500

    commit tree 844364084ecc68584dfb5473502c4b8563c2c0bb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
